1c1e7af1f78705b93a5e755b3e63ddc90f23eadf
Sasha Hurbanava <sasha.hurbnva@gmail.com>
2025-07-08 14:37:08 +0300

    fix: ESLint, Prettier configs, and package scripts

--- rs-react-app/eslint.config.js
+++ rs-react-app/eslint.config.js
@@ -2,20 +2,17 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
-import { globalIgnores } from 'eslint/config';
-import eslintPluginPrettier from 'eslint-plugin-prettier';
 import react from 'eslint-plugin-react';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+import reactCompiler from 'eslint-plugin-react-compiler';
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+export default tseslint.config(
+  { ignores: ['dist'] },
   {
-    files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.strict,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
+      ...tseslint.configs.strict,
       eslintPluginPrettier,
     ],
     files: ['**/*.{ts,tsx}'],
@@ -44,5 +41,5 @@ export default tseslint.config([
         version: 'detect',
       },
     },
-  },
-]);
+  }
+);
