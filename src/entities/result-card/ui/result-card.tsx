0f01ebd1f484499801371ace14fb98c55189f56e
Sasha Hurbanava <sasha.hurbnva@gmail.com>
2025-07-21 16:17:30 +0300

    refactor: migrate the ResultCard component from a class to a functional component

--- src/entities/result-card/ui/result-card.tsx
+++ src/entities/result-card/ui/result-card.tsx
@@ -1,27 +1,23 @@
-import React from 'react';
-
 import type { CardProps } from '../model/interfaces';
 
 import './result-card.scss';
 
-export default class ResultCard extends React.Component<CardProps> {
-  render = () => {
-    return (
-      <div className="card">
-        <h4 className="card-title">
-          {this.props.title || this.props.name || 'No title'}
-        </h4>
-        <ul>
-          {this.props.description.map((desc, index) =>
-            Object.entries(desc).map(([key, value]) => (
-              <li className="card__list-item" key={`${index}-${key}`}>
-                <span className="list-item__prop-name">{key}:</span>{' '}
-                <span>{value}</span>
-              </li>
-            ))
-          )}
-        </ul>
-      </div>
-    );
-  };
-}
+const resultCard = (props: CardProps) => {
+  return (
+    <div className="card">
+      <h4 className="card-title">{props.title || props.name || 'No title'}</h4>
+      <ul>
+        {props.description.map((desc, index) =>
+          Object.entries(desc).map(([key, value]) => (
+            <li className="card__list-item" key={`${index}-${key}`}>
+              <span className="list-item__prop-name">{key}:</span>{' '}
+              <span>{value}</span>
+            </li>
+          ))
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export default resultCard;
